7727c892e2dec5beee92a59b491a5d65ec9088a5
笨小孩 <3134376267@qq.com>
2022-04-20 11:17:28 +0800

    JDBC API 详解：Statement

---
+++ jdbc/src/main/java/priv/ljf/jdbc_/statement/TestDDL.java
@@ -0,0 +1,33 @@
+package priv.ljf.jdbc_.statement;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * 执行DDL语句
+ * @throws Exception
+ */
+@SuppressWarnings({"all"})
+public class TestDDL {
+    public static void main(String[] args) throws SQLException {
+        //1.注册驱动
+        //2.获取连接：如果连接的是本机mysql并且端口是默认的 3306 可以简化书写
+        String url = "jdbc:mysql:///mydb6_view";
+        String username = "root";
+        String password = "ljf684684";
+        Connection connection = DriverManager.getConnection(url, username, password);
+        //3.定义sql语句
+        String sql = "drop table salgrade";
+        //4.获取执行sql的对象
+        Statement statement = connection.createStatement();
+        //5.执行sql
+        int i = statement.executeUpdate(sql);
+        //6.处理结果
+        System.out.println(i);
+        //7.释放资源
+        statement.close();
+        connection.close();
+    }
+}
---
+++ jdbc/src/main/java/priv/ljf/jdbc_/statement/TestDML.java
@@ -0,0 +1,35 @@
+package priv.ljf.jdbc_.statement;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+/**
+ * 执行DML语句
+ */
+@SuppressWarnings({"All"})
+public class TestDML {
+    public static void main(String[] args) throws Exception {
+        //1.注册驱动
+        //2.获取连接
+        String url = "jdbc:mysql:///mybatis";
+        String username = "root";
+        String password = "ljf684684";
+        Connection connection = DriverManager.getConnection(url, username, password);
+        //3.定于sql语句
+        String sql = "update tb_user set username = 'lisi' where id = 2";
+        //4.获取执行sql的对象
+        Statement statement = connection.createStatement();
+        //5.执行sql
+        int i = statement.executeUpdate(sql);
+        //6.处理结果
+        if (i > 0){
+            System.out.println("修改成功");
+        }else {
+            System.out.println("修改失败");
+        }
+        //7.释放资源
+        statement.close();
+        connection.close();
+    }
+}
