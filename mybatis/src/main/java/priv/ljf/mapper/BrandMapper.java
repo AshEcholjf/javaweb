80dd6e11c73eb91cfbf6b0c1bde7513add8ba2d1
笨小孩 <3134376267@qq.com>
2022-04-21 15:18:06 +0800

    Mybatis查询所有数据

---
+++ mybatis/src/main/java/priv/ljf/mapper/BrandMapper.java
@@ -0,0 +1,9 @@
+package priv.ljf.mapper;
+
+import priv.ljf.pojo.Brand;
+
+import java.util.List;
+
+public interface BrandMapper {
+    public List<Brand>  selectAll();
+}
--- mybatis/src/test/java/priv/ljf/test/MybatisTest.java
+++ mybatis/src/test/java/priv/ljf/test/MybatisTest.java
@@ -1,4 +1,33 @@
 package priv.ljf.test;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+import priv.ljf.mapper.BrandMapper;
+import priv.ljf.pojo.Brand;
+
+import java.io.InputStream;
+import java.util.List;
+
 public class MybatisTest {
+
+    @Test
+    public void testSelectAll()throws Exception{
+        //1.获取SqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        //2.获取SqlSession对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        //3.获取Mapper接口的代理对象
+        BrandMapper mapper = sqlSession.getMapper(BrandMapper.class);
+        //4.执行方法
+        List<Brand> brands = mapper.selectAll();
+        System.out.println(brands);
+        //5.释放资源
+        sqlSession.close();
+    }
 }
