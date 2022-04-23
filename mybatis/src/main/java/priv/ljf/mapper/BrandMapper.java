80cae0a9cef43a1b939885fe535e128081bdfb70
笨小孩 <3134376267@qq.com>
2022-04-23 22:03:22 +0800

    多条件查询

--- mybatis/src/main/java/priv/ljf/mapper/BrandMapper.java
+++ mybatis/src/main/java/priv/ljf/mapper/BrandMapper.java
@@ -1,8 +1,10 @@
 package priv.ljf.mapper;
 
+import org.apache.ibatis.annotations.Param;
 import priv.ljf.pojo.Brand;
 
 import java.util.List;
+import java.util.Map;
 
 public interface BrandMapper {
     /**
@@ -13,4 +15,21 @@ public interface BrandMapper {
      * 查询详情:根据id查询
      */
     Brand selectById(int id);
+
+    /**
+     * 条件查询：
+     *         *参数接受
+     *              1.  散装参数：如果方法中有多个参数，需要使用@param("sql参数占位符名称")
+     *              2.对象参数：将多个参数封装成一个 实体对象 ，将该实体对象作为接口的方法参数。
+     *              3.map集合参数：将多个参数封装到map集合中，将map集合作为接口的方法参数。
+     */
+//    使用 `@Param("参数名称")` 标记每一个参数，在测试中调用对应的方法，传入参数，sql语句会根据@param标记的名称一一对应
+//    List<Brand> selectByCondition(@Param("status")int status,@Param("companyName") String companyName,@Param("brandName") String brandName);
+    //该方式要求在映射配置文件的SQL中使用 `#{内容}` 时，里面的内容必须和实体类属性名保持一致。这样在执行sql语句时才能根据占位符里的参数找到对象中对应的参数
+//    List<Brand> selectByCondition(Brand brand);
+    //该方式要求在映射配置文件的SQL中使用 `#{内容}` 时，里面的内容必须和map集合中键的名称一致。以便sql语句根据键找到对应的值
+    List<Brand> selectByCondition(Map map);
+
+
+
 }
--- mybatis/src/test/java/priv/ljf/test/MybatisTest.java
+++ mybatis/src/test/java/priv/ljf/test/MybatisTest.java
@@ -9,7 +9,9 @@ import priv.ljf.mapper.BrandMapper;
 import priv.ljf.pojo.Brand;
 
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MybatisTest {
 
@@ -48,5 +50,48 @@ public class MybatisTest {
         System.out.println(brand);
         //5.释放资源
         sqlSession.close();
+    }
+
+    //条件查询测试
+    @Test
+    public void testByCondition()throws Exception{
+        //接受用户输入的参数
+        int status = 1;
+        String companyName = "华为";
+        String brandName = "华为";
+
+        //对参数进行处理
+        companyName = "%" + companyName + "%";
+        brandName = "%" + brandName + "%";
+
+        Brand brand = new Brand();
+        brand.setBrandName(brandName);
+        brand.setCompanyName(companyName);
+        brand.setStatus(status);
+
+        //1.获取sqlSessionFactory对象加载mybatis配置文件
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        //2.获取sqlSession对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        //3.获取Mapper接口代理对象，用来做执行对应的sql方法
+        BrandMapper mapper = sqlSession.getMapper(BrandMapper.class);
+        //4.执行sql语句
+        //方式一 ：接口方法参数使用 @Param 方式调用的方法
+//        List<Brand> brands = mapper.selectByCondition(status, companyName, brandName);
+        //方式二 ：接口方法参数是 实体类对象 方式调用的方法
+//        List<Brand> brands = mapper.selectByCondition(brand);
+
+         //方式三 ：接口方法参数是 map集合对象 方式调用的方法
+        Map<Object, Object> map = new HashMap<>();
+        map.put("status",status);
+        map.put("companyName",companyName);
+        map.put("brandName",brandName);
+        List<Brand> brands = mapper.selectByCondition(map);
+        System.out.println(brands);
+        //5.释放资源
+        sqlSession.close();
+
     }
 }
