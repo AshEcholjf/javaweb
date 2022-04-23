29fd59911d3bfd200fd31645f31416be5f689292
笨小孩 <3134376267@qq.com>
2022-04-23 21:28:35 +0800

    查询详情

--- mybatis/src/main/java/priv/ljf/mapper/BrandMapper.java
+++ mybatis/src/main/java/priv/ljf/mapper/BrandMapper.java
@@ -5,5 +5,12 @@ import priv.ljf.pojo.Brand;
 import java.util.List;
 
 public interface BrandMapper {
-    public List<Brand>  selectAll();
+    /**
+     * 查询所有
+     */
+    List<Brand> selectAll();
+    /**
+     * 查询详情:根据id查询
+     */
+    Brand selectById(int id);
 }
--- mybatis/src/test/java/priv/ljf/test/MybatisTest.java
+++ mybatis/src/test/java/priv/ljf/test/MybatisTest.java
@@ -30,4 +30,23 @@ public class MybatisTest {
         //5.释放资源
         sqlSession.close();
     }
+    @Test
+    public void testById()throws Exception{
+        //接受页面传入的id参数
+        int id = 1;
+        //1.获取SqlSessionFactory对象，加载Mybatis配置文件
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        //2.获取SqlSession对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        //3.获取Mapper接口代理对象，用来执行sql语句
+        BrandMapper mapper = sqlSession.getMapper(BrandMapper.class);
+        //4.执行方法(sql语句)
+        Brand brand = mapper.selectById(id);
+        System.out.println(brand);
+        //5.释放资源
+        sqlSession.close();
+    }
 }
